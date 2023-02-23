1ffc1571fac7b4bb1ff4ebda408edfb215f7b45b
david00154 <davidbriggs00154@gmail.com>
2023-02-23 03:09:07 +0100

    latest -commit

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use((req, res, next) => {
-	// console.log(req.originalUrl);
-	let urlEndsWithSlash = req.url.endsWith("/");
-	let urlEndsWithSlash2 = req.originalUrl.endsWith("/");
-	if (!urlEndsWithSlash || !urlEndsWithSlash2) {
-		req.url = req.url + "/";
-		req.originalUrl = req.originalUrl + "/";
-		next();
-	} else {
-		next();
-	}
-});
+// app.use((req, res, next) => {
+// 	// console.log(req.originalUrl);
+// 	let urlEndsWithSlash = req.url.endsWith("/");
+// 	let urlEndsWithSlash2 = req.originalUrl.endsWith("/");
+// 	if (!urlEndsWithSlash || !urlEndsWithSlash2) {
+// 		req.url = req.url + "/";
+// 		req.originalUrl = req.originalUrl + "/";
+// 		next();
+// 	} else {
+// 		next();
+// 	}
+// });
 app.use(
-	"/*",
+	"*",
 	createProxyMiddleware({
 		target: "https://x11.horizonexpress.online/",
 		changeOrigin: true,
 		pathRewrite: {
-			"^/admin": "/admin/",
+			"/admin": "/admin/",
 		},
 	})
 );
 
-const PORT = process.env.PORT || 8007;
+const PORT = process.env.PORT || 8005;
 
 app.listen(PORT, () => {
 	console.log("Server started on http://localhost:%s", PORT);
